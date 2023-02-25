cf672fe4d42011480e313c5d28efee5ac63d26b3
caoLei1997 <caoLei1997@126.com>
2023-02-25 13:16:35 +0800

    feat:delete node module

--- node_modules/.pnpm/@better-scroll+indicators@2.5.0/node_modules/@better-scroll/indicators/src/__mocks__/indicator.ts
+++
@@ -1,9 +0,0 @@
-const mockIndicator = jest
-  .fn()
-  .mockImplementation(function IndicatorMockFn(scroll: any, options: any) {
-    return {
-      destroy: jest.fn(),
-    }
-  })
-
-export default mockIndicator
